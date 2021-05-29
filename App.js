c9581cc0235bc0d64dee7add4e6c0a5f9e6d2c68
Gustavo Angelo <gustavolz.angelo@gmail.com>
2021-05-29 12:38:26 -0300

    screen based on safeviewarea created

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { View } from 'react-native';
+import MessagesScreen from './app/screens/MessagesScreen';
 
 import ViewImageScreen from './app/screens/ViewImageScreen';
 export default function App() {
-  return <ViewImageScreen />;
+  return <MessagesScreen />;
 }
