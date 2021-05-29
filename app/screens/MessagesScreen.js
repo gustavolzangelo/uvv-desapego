f530a4b6bf7017ddba88f83f1f48c98009bf0ac1
Gustavo Angelo <gustavolz.angelo@gmail.com>
2021-05-29 13:50:48 -0300

    swipe listItem delete

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { FlatList, StyleSheet } from 'react-native';
+import { FlatList, StyleSheet, View } from 'react-native';
 
 import ListItem from '../components/ListItem';
+import ListItemDeleteAction from '../components/ListItemDeleteAction';
 import ListItemSeparator from '../components/ListItemSeparator';
 import Screen from '../components/Screen';
 
@@ -32,6 +33,7 @@ function MessagesScreen() {
             subtitle={item.description}
             image={item.image}
             onPress={() => console.log('message', item)}
+            renderRightActions={ListItemDeleteAction}
           />
         )}
         ItemSeparatorComponent={ListItemSeparator}
