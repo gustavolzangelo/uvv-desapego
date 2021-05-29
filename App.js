af360c477f3206e354163e5f8cca6e64ccdca5cd
Gustavo Angelo <gustavolz.angelo@gmail.com>
2021-05-29 12:23:52 -0300

    ListItem, ListingDetailsScreen created and ViewImageScreen modified

--- App.js
+++ App.js
@@ -1,20 +1,7 @@
 import React from 'react';
 import { View } from 'react-native';
 
-import Card from './app/components/Card';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 export default function App() {
-  return (
-    <View
-      style={{
-        backgroundColor: '#f8f4f4',
-        padding: 20,
-        paddingTop: 100,
-      }}>
-      <Card
-        title="Jaqueta vermelha para vender"
-        subTitle="R$ 100"
-        image={require('./app/assets/jacket-red.jpg')}
-      />
-    </View>
-  );
+  return <ViewImageScreen />;
 }
---
+++ app/components/ListItem.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { View, StyleSheet, Image } from 'react-native';
+import colors from '../config/colors';
+import AppText from './AppText';
+
+function ListItem({ title, subtitle, image }) {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.image} source={image} />
+      <View>
+        <AppText style={styles.title}>{title}</AppText>
+        <AppText style={styles.subTitle}>{subtitle}</AppText>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+  },
+  image: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    marginRight: 10,
+  },
+  subTitle: {
+    color: colors.medium,
+  },
+  title: {
+    fontWeight: '500',
+  },
+});
+
+export default ListItem;
