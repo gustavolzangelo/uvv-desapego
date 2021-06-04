78e8038fb24c455b4a39972d044a8f336f41a05f
Gustavo Angelo <gustavolz.angelo@gmail.com>
2021-06-04 20:05:17 -0300

    text input  configured

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import { View } from 'react-native';
-import Icon from './app/components/Icon';
-import ListItem from './app/components/ListItem';
+import React, { useState } from 'react';
+import AppPicker from './app/components/AppPicker';
 import Screen from './app/components/Screen';
-import AccountScreen from './app/screens/AccountScreen';
-import ListingScreen from './app/screens/ListingScreen';
-import MessagesScreen from './app/screens/MessagesScreen';
 
-import ViewImageScreen from './app/screens/ViewImageScreen';
+const categories = [
+  { label: 'Furniture', value: 1 },
+  { label: 'Clothing', value: 2 },
+];
+
 export default function App() {
-  return <ListingScreen />;
+  const [category, setCategory] = useState(categories[0]);
+
+  return (
+    <Screen>
+      <AppPicker
+        selectedItem={category}
+        onSelectItem={(item) => setCategory(item)}
+        items={categories}
+        placeholder="Category"
+        icon={'mail'}
+      />
+    </Screen>
+  );
 }
--- app/components/AppText.js
+++ app/components/AppText.js
@@ -1,27 +1,14 @@
 import React from 'react';
-import { Text, Platform, StyleSheet } from 'react-native';
+import { Text } from 'react-native';
+
+import defaultStyles from '../config/styles';
 
 function AppText({ children, style, ...otherProps }) {
   return (
-    <Text style={[styles.text, style]} {...otherProps}>
+    <Text style={[defaultStyles.text, style]} {...otherProps}>
       {children}
     </Text>
   );
 }
-
-const styles = StyleSheet.create({
-  text: {
-    ...Platform.select({
-      ios: {
-        fontSize: 20,
-        fontFamily: 'Avenir',
-      },
-      android: {
-        fontSize: 18,
-        fontFamily: 'Roboto',
-      },
-    }),
-  },
-});
 
 export default AppText;
