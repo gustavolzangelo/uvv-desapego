283c9818e71da30d603f9ed548d299ecf791c224
Gustavo Angelo <gustavolz.angelo@gmail.com>
2021-06-05 17:44:07 -0300

    additional config

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
 import * as Yup from 'yup';
+import CategoryPickerItem from '../components/CategoryPickerItem';
 
 import { AppForm, AppFormField, AppFormPicker, SubmitButton } from '../components/forms';
 import Screen from '../components/Screen';
@@ -13,9 +14,9 @@ const validationSchema = Yup.object().shape({
 });
 
 const categories = [
-  { label: 'Furniture', value: 1 },
-  { label: 'Clothing', value: 2 },
-  { label: 'Camera', value: 3 },
+  { label: 'Furniture', value: 1, backgroundColor: 'red', icon: 'apps' },
+  { label: 'Clothing', value: 2, backgroundColor: 'green', icon: 'email' },
+  { label: 'Camera', value: 3, backgroundColor: 'blue', icon: 'lock' },
 ];
 
 function ListingEditScreen() {
@@ -31,8 +32,21 @@ function ListingEditScreen() {
         onSubmit={(values) => console.log(values)}
         validationSchema={validationSchema}>
         <AppFormField maxLength={255} name="title" placeholder="Title" />
-        <AppFormField keyboardType="numeric" maxLength={8} name="price" placeholder="Price" />
-        <AppFormPicker items={categories} name="category" placeholder="Category" />
+        <AppFormField
+          keyboardType="numeric"
+          maxLength={8}
+          name="price"
+          placeholder="Price"
+          width={120}
+        />
+        <AppFormPicker
+          items={categories}
+          name="category"
+          numberOfColumns={3}
+          PickerItemComponent={CategoryPickerItem}
+          placeholder="Category"
+          width={'25%'}
+        />
         <AppFormField
           maxLength={255}
           multiline
