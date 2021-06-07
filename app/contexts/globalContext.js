ede101f28d59ce104f4e5147b6b3967722a0291b
Gustavo Angelo <gustavolz.angelo@gmail.com>
2021-06-06 21:30:25 -0300

    login context

--- app/contexts/globalContext.js
+++ app/contexts/globalContext.js
@@ -4,7 +4,7 @@ import { useState } from 'react/cjs/react.development';
 const GlobalContext = createContext();
 
 export const GlobalProvider = ({ children }) => {
-  const [signed, setSigned] = useState(true);
+  const [signed, setSigned] = useState(false);
 
   return <GlobalContext.Provider value={{ signed, setSigned }}>{children}</GlobalContext.Provider>;
 };
--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { StyleSheet, Image } from 'react-native';
 import * as Yup from 'yup';
 
 import Screen from '../components/Screen';
 import { AppFormField, AppForm, SubmitButton } from '../components/forms';
+import GlobalContext from '../contexts/globalContext';
 
 const validationSchema = Yup.object().shape({
   email: Yup.string().required().email().label('Email'),
@@ -11,13 +12,17 @@ const validationSchema = Yup.object().shape({
 });
 
 function LoginScreen() {
+  const context = useContext(GlobalContext);
+
+  const handleSubmit = () => context.setSigned(true);
+
   return (
     <Screen style={styles.container}>
       <Image style={styles.logo} source={require('../assets/logo-red.png')} />
 
       <AppForm
         initialValues={{ email: '', password: '' }}
-        onSubmit={(values) => console.log(values)}
+        onSubmit={handleSubmit}
         validationSchema={validationSchema}>
         <AppFormField
           autoCapitalize="none"
